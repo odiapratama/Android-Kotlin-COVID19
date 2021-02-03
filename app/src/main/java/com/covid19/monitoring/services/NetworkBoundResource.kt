db64fe1981c271607260a83c964964453a20de88
telkomdev-odiapratama <odiapratama@gmail.com>
2021-02-03 10:10:03 +0700

    Network bound improvement

--- app/src/main/java/com/covid19/monitoring/services/NetworkBoundResource.kt
+++ app/src/main/java/com/covid19/monitoring/services/NetworkBoundResource.kt
@@ -2,17 +2,18 @@ package com.covid19.monitoring.services
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.MutableLiveData
-import com.covid19.monitoring.di.CoroutineContextProviders
 import com.covid19.monitoring.utils.isOnline
 import com.covid19.monitoring.utils.launchOn
 import com.covid19.monitoring.utils.safeApiCall
+import kotlinx.coroutines.launch
 
-abstract class NetworkBoundResource<ResultType, RequestType>(
+abstract class NetworkBoundResource<RequestType, ResultType>(
     private val coroutineContext: CoroutineContextProviders
 ) {
     val data = MutableLiveData<Resource<ResultType>>()
 
-    abstract fun loadFromDB(): ResultType?
+    @WorkerThread
+    abstract suspend fun loadFromDB(): ResultType?
 
     abstract fun setLatestFetch()
 
@@ -21,38 +22,39 @@ abstract class NetworkBoundResource<ResultType, RequestType>(
     abstract fun shouldFetchData(data: ResultType?, fetchDate: String): Boolean
 
     @WorkerThread
-    abstract suspend fun remoteCall(): ResultType
+    abstract suspend fun remoteCall(): RequestType
 
-    abstract fun saveRemoteData(data: ResultType)
+    @WorkerThread
+    abstract suspend fun saveRemoteData(data: RequestType)
 
-    abstract fun clearData()
+    @WorkerThread
+    abstract suspend fun clearData()
 
     init {
         execute()
     }
 
     private fun execute() {
-        val dataDb = loadFromDB()
-        val dateFetchDate = getFetchDate(dataDb)
-        data.postValue(Resource.loading(dataDb))
-        if (isOnline()) {
-            if (shouldFetchData(dataDb, dateFetchDate)) fetchRemote()
-            else data.postValue(Resource.success(dataDb))
-        } else data.postValue(Resource.cached(dataDb))
-    }
-
-    private fun fetchRemote() {
         launchOn(coroutineContext.io) {
-            safeApiCall({
-                remoteCall().also { response ->
-                    data.postValue(Resource.success(response))
-                    saveRemoteData(response)
-                    setLatestFetch()
-                }
-            }, {
-                clearData()
-                data.postValue(Resource.error(it.message ?: "Unexpected error"))
-            })
+            launch {
+                val dataDb = loadFromDB()
+                val dateFetchDate = getFetchDate(dataDb)
+                data.postValue(Resource.loading(dataDb))
+                if (isOnline()) {
+                    if (shouldFetchData(dataDb, dateFetchDate)) {
+                        safeApiCall({
+                            remoteCall().also { response ->
+                                saveRemoteData(response)
+                                data.postValue(Resource.success(loadFromDB()))
+                                setLatestFetch()
+                            }
+                        }, {
+                            launch { clearData() }
+                            data.postValue(Resource.error(it.message ?: "Unexpected error"))
+                        })
+                    } else data.postValue(Resource.success(dataDb))
+                } else data.postValue(Resource.cached(dataDb))
+            }
         }
     }
 }
